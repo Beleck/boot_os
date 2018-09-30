f53d9a9ebb20b103e09356d1d37ce050c33c350e
Thomas Beck <thomas.beck@live.fr>
2018-09-30 17:30:55 -0300

    Add stack for functions calls

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ IMG=$(BOOT)/bin/bootloader.bin
 MAKE_OPTS=--no-print-directory
 
 QEMU=qemu-system-x86_64
-QEMU_OPTS=-fda $(IMG)
+QEMU_OPTS=-drive format=raw,file=$(IMG) 
 
 .PHONY: all
 all: boot
@@ -14,7 +14,7 @@ boot:
 	@$(MAKE) $(MAKE_OPTS) all -C boot/
 
 .PHONY: run
-run:
+run: all
 	$(QEMU) $(QEMU_OPTS)
 
 .PHONY: clean
--- boot/bootloader.asm
+++ boot/bootloader.asm
@@ -1,45 +1,60 @@
 bits 16 ;real mode is in 16 bits
-org 0x7C00
+;org 0x7C00
 
 
 start:
+    mov ax, 0x7c0
+    add ax, 0x20 ; bootloader size
+    mov ss, ax
+    mov sp, 4096
+
+    mov ax, 0x7c0
+    mov ds, ax
+
+    call clear_screen
+    call reset_cursor
+
+    mov si, msg
+    call print
     cli
+    hlt
+
+print:
+    mov ah, 0x0E
+.print_char: 
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .print_char
+.done:
+    ret
 
 reset_cursor:
     mov ah, 0x02
-    mov dh, 0
-    mov dl, 0
-    int 0x10
-
-delete_screen:
-    mov al, ' '
-    mov ah, 0x0E
-.erase int 0x10
-    add bh, 1
-    sub bh, 255
-    jne .erase
     mov bh, 0
-    add bl, 1
-    sub bl, 3
-    jne .erase
-
-    mov ah, 0x02
     mov dh, 0
     mov dl, 0
     int 0x10
+    ret
 
-send_message:
-    mov si, msg
-    mov ah, 0x0E
-.loop lodsb
-    or al, al
-    jz halt
+clear_screen:
+    call reset_cursor
+    mov al, ' '
+    mov ah, 0x0e
+    mov bh, 0
+    mov ch, 0
+.erase:
     int 0x10
-    jmp .loop
+    add bh, 1
+    cmp bh, 255
+    jne .erase
+    add ch, 1
+    cmp ch, 4
+    jne .erase
+    ret
 
-halt: hlt
-
-msg: db "Next generation bootloader of the street say : welcome", 0
+msg: db "Booting from boot_os...", 0
 
 ;fill 512 bytes with 0 and finish by magic numbers
 times 510 - ($ - $$) db 0
