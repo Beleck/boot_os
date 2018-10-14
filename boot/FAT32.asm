5586c0fd00848dfdd8c6a3931773bb840516e6a3
Thomas Beck <thomas.beck@live.fr>
2018-10-14 20:28:56 -0300

    Add FAT32 boot sector
    It works on my computer

---
+++ boot/FAT32.asm
@@ -0,0 +1,34 @@
+;FAT32 Header
+;https://en.wikipedia.org/wiki/Design_of_the_FAT_file_system
+
+jmp short start
+nop
+
+oem_name              db 		"BOOT--OS"
+bytes_per_sector      dw 		0x0200
+sectors_per_cluster   db 		0x08
+reserved_sectors      dw 		0x0020
+total_FAT             db 		0x02
+max_root_dir          dw 		0x0000
+logical_sectors       dw 		0x0000
+media_descriptor      db 		0xF8
+sectors_per_FAT       dw 		0x0000
+sectors_per_track     dw 		0x003D
+sectors_per_head      dw 		0x0002
+hidden_sectors        dd 		0x00000000
+total_sectors    	  dd 		0x00FE3B1F		
+sectors_per_FAT32     dd 		0x00000778
+flags                 dw 		0x0000
+FS_version            dw 		0x0000
+root_directory_start  dd 		0x00000002
+FS_info_sector        dw 		0x0001
+copy_boot_sector      dw 		0x0006
+
+TIMES 12 DB 0 
+
+drive_number          db 		0x00
+reserved_byte         db   	    0x00
+signature             db 		0x29
+volume_ID             dd 		0xFFFFFFFF
+volume_label          db 		"BOOT OS    "
+system_ID              db 		"FAT32   "
--- boot/bootloader.asm
+++ boot/bootloader.asm
@@ -1,12 +1,18 @@
 bits 16 ;real mode is in 16 bits
-org 0x7C00
+org 0x0000
 
+%include 'FAT32.asm'
 
 start:
+    cli
     mov ax, 0x7c0
-    add ax, 0x20 ; bootloader size
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ax, 0x0000
     mov ss, ax
-    mov sp, 4096
+    mov sp, 0xffff
 
     call clear_screen
     call reset_cursor
@@ -14,12 +20,12 @@ start:
     mov si, msg
     call print
     call a20_routine
-    cli
     hlt
 
 %include 'data.asm'
 %include 'api.asm'
 %include 'a20.asm'
+
 
 ;fill 512 bytes with 0 and finish by magic numbers
 times 510 - ($ - $$) db 0
